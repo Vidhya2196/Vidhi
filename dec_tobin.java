c289593667f937921e1fb8dfa4b9bff6465e9b58
Vidhya.R <vidhya2196.kpr@gmail.com>
2016-07-03 13:38:23 +0530

    Create dec_tobin.java

---
+++ dec_tobin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i;
+	Scanner input=new Scanner(System.in);
+	i=input.nextInt();
+	System.out.print(Integer.toBinaryString(i));
+	}
+}
